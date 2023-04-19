9651332ef8f87648ae23f1c6f9fdbd6aa4b79514
Miqdad <miqdaddeveloper@gmail.com>
2023-04-19 14:38:41 +0700

    (feat) : Add MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:myquran/bloc/ayat/ayat_cubit.dart';
+import 'package:myquran/bloc/surat/surat_cubit.dart';
+import 'package:myquran/data/services/quran_service.dart';
+import 'package:http/http.dart' as http;
 import 'ui/home_page.dart';
 
 void main() {
@@ -14,18 +18,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => SuratCubit(
+                QuranService(
+                  client: http.Client(),
+                ),
+              ),
+            ),
+            BlocProvider(
+              create: (context) => AyatCubit(
+                QuranService(
+                  client: http.Client(),
+                ),
+              ),
+            ),
+          ],
+          child: MaterialApp(
             title: 'My Quran',
             theme: ThemeData(
               primarySwatch: Colors.brown,
               fontFamily: GoogleFonts.poppins().fontFamily,
             ),
             home: const HomePage(),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
