e1f131fb8e5747f691088253404140ee9c4d58b3
Miqdad <miqdaddeveloper@gmail.com>
2023-04-19 14:38:22 +0700

    (feat) : Add Navigation

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:myquran/ui/surat_page.dart';
 import 'const.dart';
 
 class HomePage extends StatefulWidget {
@@ -113,59 +114,73 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Column(
                   children: [
-                    Container(
-                      width: (size.width - 24.sp - 24.sp - 20.sp) / 2,
-                      height: 186.h,
-                      decoration: BoxDecoration(
-                        color: AppColors.primary,
-                        borderRadius: BorderRadius.circular(24.r),
-                        border: Border.all(
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const SuratPage();
+                            },
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: (size.width - 24.sp - 24.sp - 20.sp) / 2,
+                        height: 186.h,
+                        decoration: BoxDecoration(
                           color: AppColors.primary,
+                          borderRadius: BorderRadius.circular(24.r),
+                          border: Border.all(
+                            color: AppColors.primary,
+                          ),
                         ),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          left: 16.sp,
-                        ),
-                        child: Stack(
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 24.sp),
-                                  child: SvgPicture.asset(
-                                    'assets/book.svg',
-                                    fit: BoxFit.fitHeight,
-                                    // color: AppColors.white,
-                                    height: 52.h,
-                                    width: 44.w,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(bottom: 24.sp),
-                                  child: Text(
-                                    'Al Quran',
-                                    style: TextStyle(
-                                      color: AppColors.white,
-                                      fontSize: 20.sp,
-                                      fontWeight: FontWeight.w800,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 16.sp,
+                          ),
+                          child: Stack(
+                            children: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 24.sp),
+                                    child: SvgPicture.asset(
+                                      'assets/book.svg',
+                                      fit: BoxFit.fitHeight,
+                                      // color: AppColors.white,
+                                      height: 52.h,
+                                      width: 44.w,
                                     ),
                                   ),
-                                )
-                              ],
-                            ),
-                            Positioned(
-                              top: -4.sp,
-                              left: 76.sp,
-                              child: SizedBox(
-                                width: 160.w,
-                                height: 160.h,
-                                child: SvgPicture.asset('assets/ornament.svg'),
+                                  Padding(
+                                    padding: EdgeInsets.only(bottom: 24.sp),
+                                    child: Text(
+                                      'Al Quran',
+                                      style: TextStyle(
+                                        color: AppColors.white,
+                                        fontSize: 20.sp,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
-                            ),
-                          ],
+                              Positioned(
+                                top: -4.sp,
+                                left: 76.sp,
+                                child: SizedBox(
+                                  width: 160.w,
+                                  height: 160.h,
+                                  child:
+                                      SvgPicture.asset('assets/ornament.svg'),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -221,8 +236,9 @@ class _HomePageState extends State<HomePage> {
                               child: SizedBox(
                                 width: 160.w,
                                 height: 160.h,
-                                child: SvgPicture.asset('assets/ornament.svg',
-                                    // color: AppColors.primary,
+                                child: SvgPicture.asset(
+                                  'assets/ornament.svg',
+                                  // color: AppColors.primary,
                                 ),
                               ),
                             ),
@@ -283,8 +299,9 @@ class _HomePageState extends State<HomePage> {
                               child: SizedBox(
                                 width: 160.w,
                                 height: 160.h,
-                                child: SvgPicture.asset('assets/ornament.svg',
-                                    // color: AppColors.primary,
+                                child: SvgPicture.asset(
+                                  'assets/ornament.svg',
+                                  // color: AppColors.primary,
                                 ),
                               ),
                             ),
