864e08734d81547564fdb0fa46e6d379db351c4f
Miqdad <miqdaddeveloper@gmail.com>
2023-04-19 14:49:29 +0700

    (feat) : Add State SuratLoading, Surat Loaded & SuratError

--- lib/bloc/surat/surat_state.dart
+++ lib/bloc/surat/surat_state.dart
@@ -1,6 +1,32 @@
 part of 'surat_cubit.dart';
 
-@immutable
-abstract class SuratState {}
+abstract class SuratState extends Equatable {
+  const SuratState();
+
+  @override
+  List<Object> get props => [];
+}
 
 class SuratInitial extends SuratState {}
+
+class SuratLoading extends SuratState {}
+
+class SuratLoaded extends SuratState {
+  final List<SuratModel> listSurat;
+
+  SuratLoaded({required this.listSurat});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [listSurat];
+}
+
+class SuratError extends SuratState{
+  final String message;
+
+  SuratError({required this.message});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [message];
+}
