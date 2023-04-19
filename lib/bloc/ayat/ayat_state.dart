4740a15f6e9a25d4d6686383eb944de8a96a8695
Miqdad <miqdaddeveloper@gmail.com>
2023-04-19 14:37:48 +0700

    (feat) : Add State AyatLoading, Ayat Loaded, & AyatError

--- lib/bloc/ayat/ayat_state.dart
+++ lib/bloc/ayat/ayat_state.dart
@@ -1,6 +1,35 @@
 part of 'ayat_cubit.dart';
 
 @immutable
-abstract class AyatState {}
+abstract class AyatState extends Equatable {
+  const AyatState();
+
+  @override
+  List<Object> get props => [];
+}
 
 class AyatInitial extends AyatState {}
+
+class AyatLoading extends AyatState {}
+
+class AyatLoaded extends AyatState {
+  final SuratDetailModel detail;
+
+  AyatLoaded({required this.detail});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [detail];
+}
+
+class AyatError extends AyatState{
+  final String message;
+
+  AyatError({required this.message});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [message];
+}
+
+
