0a73e4d661f20bf4540ab6b07d38226f56db4614
Miqdad <miqdaddeveloper@gmail.com>
2023-04-19 14:36:54 +0700

    (feat) : Add getDetailSurat function

--- lib/bloc/ayat/ayat_cubit.dart
+++ lib/bloc/ayat/ayat_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:myquran/data/models/surat_detail_model.dart';
+import 'package:myquran/data/services/quran_service.dart';
 
 part 'ayat_state.dart';
 
 class AyatCubit extends Cubit<AyatState> {
-  AyatCubit() : super(AyatInitial());
+  AyatCubit(this.quranService) : super(AyatInitial());
+
+  final QuranService quranService;
+
+  void getDetailSurat(int noSurat) async {
+    emit(AyatLoading());
+    final result = await quranService.getDetailSurat(noSurat);
+    result.fold(
+        (error) => emit(AyatError(message: error)), (r) => emit(AyatLoaded(detail: r)));
+  }
 }
